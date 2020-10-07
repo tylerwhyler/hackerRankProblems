9245854d914d3f4bde8a76f2d8b823b0af5fa5dc
tylerwhyler <tyjiman@gmail.com>
2020-10-07 07:34:21 -0700

    first attempt at problem

---
+++ plusMinus.js
@@ -0,0 +1,27 @@
+// Given an array of integers, calculate the ratios of its elements that are positive,
+// negative, and zero. Print the decimal value of each fraction on a new
+// line with 6 places after the decimal.
+
+function plusMinus(arr) {
+    let minusCount;
+    let plusCount;
+    let zeroCount;
+
+    arr.forEach(i => {
+        if (i < 0) {
+            minusCount++;
+        }
+        if (i > 0) {
+            plusCount++;
+        }
+        if ((i = 0)) {
+            zeroCount++;
+        }
+    });
+    console.log(plusCount / arr.length);
+    console.log(minusCount / arr.length);
+    console.log(zeroCount / arr.length);
+    return;
+}
+
+plusMinus([1, 1, 0, -1, -1]);
