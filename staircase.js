8bb522ab987db590d854ef9b8f8d858de56b0ef7
tylerwhyler <tyjiman@gmail.com>
2020-10-08 11:29:20 -0700

    staircase problem took my about an hour, way longer than it should have

---
+++ staircase.js
@@ -0,0 +1,18 @@
+function staircase(n) {
+    let hashvar = '';
+    for (let i = 0; i < n; i++) {
+        hashvar += ' ';
+    }
+    let hashvarArr = hashvar.split('');
+    for (let i = 0; i < n; i++) {
+        for (let x = 0; x <= i; x++) {
+            hashvarArr.pop();
+        }
+        for (let x = 0; x <= i; x++) {
+            hashvarArr.push('#');
+        }
+        console.log(hashvarArr.join(''));
+    }
+}
+
+staircase(4);
