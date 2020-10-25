c51ea5552d0dc3dc2a057de817348338ec78b0a3
tylerwhyler <tyjiman@gmail.com>
2020-10-24 22:59:06 -0700

    note to self

---
+++ javascriptScope.js
@@ -0,0 +1,4 @@
+console.log(Object.prototype)
+
+// Learn how javascript functions on behind the scenes, event loop, class vs prototype, why javascript isn't technically
+// object orientet. Deep understanding is key, always.
