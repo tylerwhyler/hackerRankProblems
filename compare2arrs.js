18f6d4fac394a9a7378735159c6933cafa258661
tylerwhyler <tyjiman@gmail.com>
2020-11-22 09:43:15 -0800

    fixed function so that it would work with arrays of any length

--- compare2arrs.js
+++ compare2arrs.js
@@ -2,7 +2,7 @@ function compareTriplets(a, b) {
     let aliceScore = 0;
     let bobScore = 0;
     
-    for (let i = 0; i <= 2; i++) {
+    for (let i = 0; i < a.length; i++) {
         if (a[i] > b[i]) {
             aliceScore++;
         } if (a[i] < b[i]) {
