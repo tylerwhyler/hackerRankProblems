49bc1dc24a2b4dd9054e112e65c438b7e71b2bc1
tylerwhyler <tyjiman@gmail.com>
2020-12-01 11:48:29 -0800

    easy hackerrank problem

---
+++ camelCaseCount.js
@@ -0,0 +1,11 @@
+function camelcase(s) {
+    let count = 1;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == s[i].toUpperCase()) {
+            count++;
+        };
+    };
+    return count;
+}
+
+camelcase('helloWorldThisShouldBeSix'); //count how many words are in a camelCase'd string
