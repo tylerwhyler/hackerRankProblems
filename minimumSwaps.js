ad10cd565a98df3f132908759239bb7b809b7318
tylerwhyler <tyjiman@gmail.com>
2020-12-04 12:41:14 -0800

    was using splice completely incorrectly, array was full of strings, and now something else is wrong. The array's elements are being swapped to create duplicates of eachother, which can't happen for this problem obviously

--- minimumSwaps.js
+++ minimumSwaps.js
@@ -3,29 +3,33 @@ function minimumSwaps(sortedArr) {
     sortedArr.sort((a, b) => a - b);
     let counter = 0;
     function synthCreate(synthArr) {
-      let newSynthArr = '';
+      let newSynthArr = [];
       sortedSynthArr = [...synthArr];
       sortedSynthArr.sort((a, b) => a - b);
       for (let x = 0; x < synthArr.length; x++) {
         if (synthArr[x] != sortedSynthArr[x]) {
-          newSynthArr += synthArr[x];
+          newSynthArr.push(synthArr[x]);
         }
       }
-      return newSynthArr.split('')
+      return newSynthArr;
     }
+
     for (let y = 0; y < arr.length; y++) {
       for (let z = 0; z < arr.length; z++) {
-        if (synthCreate(arr.splice(arr[y], arr[z])).length < synthCreate(arr).length - 1) {
-          console.log(arr, '||', y, z)
-          arr.splice(arr[y], arr[z]);
+        if (synthCreate(arr.splice(y, 1, arr[z])).length < synthCreate(arr).length - 1) {
+          console.log(arr, '|', y, z)
+          arr.splice(y, 1, arr[z])
+          console.log(arr, '|', y, z)
           counter++;
-        } else if (synthCreate(arr.splice(arr[y], arr[z])).length < synthCreate(arr).length) {
-          arr.splice(arr[y], arr[z]);
+        } else if (synthCreate(arr.splice(y, 1, arr[z])).length < synthCreate(arr).length) {
+          console.log(arr, '|', y, z)
+          arr.splice(y, 1, arr[z])
+          console.log(arr, '|', y, z)
           counter++;
         } 
       }
     }
-    
+
 
     return counter;
 }
