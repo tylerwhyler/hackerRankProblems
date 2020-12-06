df4e0e95557ab8bbb7efa04932d0e7e09146976d
tylerwhyler <tyjiman@gmail.com>
2020-12-06 00:42:27 -0800

    cut the number of iterations down by another 50% by checking during the building of the synthetic array if the new array with the potential swap is already 2 or 1 elements shorter

--- minimumSwaps.js
+++ minimumSwaps.js
@@ -3,22 +3,38 @@ function minimumSwaps(sortedArr) {
     sortedArr.sort((a, b) => a - b);
     let newcount = 0;
     let counter = 0;
-    function synthCreate(synthArr) {
+    function synthCreate(synthArr, originalArr, biFlip) {
       let incorrectNums = 0;
+      let incorrectOriginalArrNums = 0;
       let sortedSynthArr = [...synthArr];
       sortedSynthArr.sort((a, b) => a - b);
+      let sortedOriginalArr = [...originalArr];
+      sortedOriginalArr.sort((a, b) => a - b);
       for (let x = 0; x < synthArr.length; x++) {
         newcount++
         console.log(newcount)
         if (synthArr[x] != sortedSynthArr[x]) {
+          console.log(newcount, "%%%")
           incorrectNums++;
         }
+        if (originalArr[x] != sortedOriginalArr[x]) {
+          console.log(newcount, "%%%")
+          incorrectOriginalArrNums++;
+        }
+        if (incorrectNums < incorrectOriginalArrNums - 1) {
+            console.log(newcount, "%%%$$$")
+          return true;
+        }
+        if (incorrectNums < incorrectOriginalArrNums && biFlip) {
+          return true;
+        }
       }
-      return incorrectNums;
+    return false;
     }
 
     while (arr.join('') != sortedArr.join('')) {
       for (let y = 0; y < arr.length; y++) {
+        if (arr.join('') === sortedArr.join('')) break;
         for (let z = 0; z < arr.length; z++) {
           if (arr.join('') === sortedArr.join('')) break;
           let arrCopy = [...arr];
@@ -28,7 +44,7 @@ function minimumSwaps(sortedArr) {
           if (arr[z] === sortedArr[z] || arr[y] === sortedArr[y]) {
              continue;
           }
-          if (synthCreate(arrCopy) < synthCreate(arr) - 1) {
+          if (synthCreate(arrCopy, arr, false)) {
             arr.splice(y, 1, arr[z])
             arr.splice(z, 1, arrCopyCopy[y])
             counter++;
@@ -44,8 +60,7 @@ function minimumSwaps(sortedArr) {
           if (arr[z] === sortedArr[z] || arr[y] === sortedArr[y]) {
             continue;
           }
-          if (synthCreate(arrCopy) < synthCreate(arr)) {
-            console.log(arr, '|', y, z)
+          if (synthCreate(arrCopy, arr, true)) {
             arr.splice(y, 1, arr[z])
             arr.splice(z, 1, arrCopyCopy[y])
             counter++;
