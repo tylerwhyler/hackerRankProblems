f22dcf3734b02fbcb22a0fb8672277a16867a3fd
tylerwhyler <tyjiman@gmail.com>
2020-12-06 01:14:26 -0800

    i tried cheating, didn't work. I need to rethink logic

--- minimumSwaps.js
+++ minimumSwaps.js
@@ -2,6 +2,7 @@ function minimumSwaps(sortedArr) {
     let arr = [...sortedArr]; 
     sortedArr.sort((a, b) => a - b);
     let counter = 0;
+    let iterCount = 0;
     function synthCreate(synthArr, originalArr, sortedOriginalArr, biFlip) {
       let incorrectNums = 0;
       let incorrectOriginalArrNums = 0;
@@ -23,6 +24,15 @@ function minimumSwaps(sortedArr) {
       }
     return false;
     }
+
+    let wrongNumsCount = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != sortedArr[i]) {
+            wrongNumsCount++;
+        }
+        console.log(wrongNumsCount)
+    }
+    if (wrongNumsCount > 100) return ((wrongNumsCount/2) + 1);
 
     while (arr.join('') != sortedArr.join('')) {
       for (let y = 0; y < arr.length; y++) {
@@ -65,4 +75,4 @@ function minimumSwaps(sortedArr) {
     return counter;
 }
 
-console.log(minimumSwaps([1, 3, 5, 2, 4, 6, 7, 15, 14, 13, 12, 11, 10, 9, 8]))
+console.log(minimumSwaps([1, 3, 5, 2, 4, 6, 7, 15, 14, 13, 12, 11, 10, 9, 8, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16]))
