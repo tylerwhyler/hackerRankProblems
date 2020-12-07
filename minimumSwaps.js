fe6bf39e3f4dc569c5626914ff27b408012e6740
tylerwhyler <tyjiman@gmail.com>
2020-12-06 18:06:09 -0800

    rewrote code after a suggestion on how to walk through the problem, MUCH more efficient but still not good enough, I passed the 10,000 element long array tests but the code is still too slow for a 100,000 element long array

--- minimumSwaps.js
+++ minimumSwaps.js
@@ -1,70 +1,26 @@
 function minimumSwaps(sortedArr) {
-    //I don't believe I can solve this without knowledge of data structures and algorithms
     let counter = 0;
     let iterCount = 0;
-    let arr = [...sortedArr]; 
+    let arr = [...sortedArr];
     sortedArr.sort((a, b) => a - b);
-    function synthCreate(synthArr, originalArr, sortedOriginalArr, biFlip) {
-      let incorrectNums = 0;
-      let incorrectOriginalArrNums = 0;
-      for (let x = 0; x < synthArr.length; x++) {
-        iterCount++;
-        console.log(iterCount)
-        if (synthArr[x] != sortedOriginalArr[x]) {
-          incorrectNums++;
-        }
-        if (originalArr[x] != sortedOriginalArr[x]) {
-          incorrectOriginalArrNums++;
-        }
-        if (incorrectNums < incorrectOriginalArrNums - 1) {
-          return true;
-        }
-        if (incorrectNums < incorrectOriginalArrNums && biFlip) {
-          return true;
-        }
-      }
-    return false;
-    }
-    
     while (arr.join('') != sortedArr.join('')) {
-      for (let y = 0; y < arr.length; y++) {
-        if (arr.join('') === sortedArr.join('')) break;
-        for (let z = 0; z < arr.length; z++) {
-          if (arr.join('') === sortedArr.join('')) break;
-          let arrCopy = [...arr];
-          let arrCopyCopy = [...arr];
-          arrCopy.splice(y, 1, arr[z]);
-          arrCopy.splice(z, 1, arrCopyCopy[y]);
-          if (arr[z] === sortedArr[z] || arr[y] === sortedArr[y]) {
-             continue;
-          }
-          if (synthCreate(arrCopy, arr, sortedArr, false)) {
-            arr.splice(y, 1, arr[z])
-            arr.splice(z, 1, arrCopyCopy[y])
-            counter++;
-            break;
-          }
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] != sortedArr[i]) {
+                let tempSwapNum = arr[i];
+                if (sortedArr[i] === arr[sortedArr.indexOf(arr[i])] && 
+                    arr[i] === sortedArr[arr.indexOf(sortedArr[i])]) {
+                    counter++;
+                    arr.splice(i, 1, sortedArr[i]);
+                    arr.splice(sortedArr.indexOf(tempSwapNum), 1, tempSwapNum);
+                    continue;
+                }
+                if (arr.join('') === sortedArr.join('')) break;
+                counter++;
+                arr.splice(arr.indexOf(sortedArr[i]), 1, arr[i]);
+                arr.splice(i, 1, sortedArr[i]);
+            }
         }
-        for (let z = 0; z < arr.length; z++) {
-          if (arr.join('') === sortedArr.join('')) break;
-          let arrCopy = [...arr];
-          let arrCopyCopy = [...arr];
-          arrCopy.splice(y, 1, arr[z]);
-          arrCopy.splice(z, 1, arrCopyCopy[y]);
-          if (arr[z] === sortedArr[z] || arr[y] === sortedArr[y]) {
-            continue;
-          }
-          if (synthCreate(arrCopy, arr, sortedArr, true)) {
-            arr.splice(y, 1, arr[z])
-            arr.splice(z, 1, arrCopyCopy[y])
-            counter++;
-          }
-        }
-      }
     }
-
 
     return counter;
 }
-
-console.log(minimumSwaps([1, 3, 5, 2, 4, 6, 7, 15, 14, 13, 12, 11, 10, 9, 8]))
