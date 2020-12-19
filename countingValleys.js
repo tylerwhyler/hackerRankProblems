f70e18f87ce3d8ceba8fb6f1c717371d328df595
tylerwhyler <tyjiman@gmail.com>
2020-12-19 01:57:32 -0800

    didn't pass all tests, and also was timing out. Made use of a switch statement instead of if else, didn't realize how much different the performance between those two methods is.

--- countingValleys.js
+++ countingValleys.js
@@ -1,19 +1,20 @@
-//counting Valleys easy hackerrank problem
 function countingValleys(steps, path) {
     // Write your code here
     let heightNum = 0;
-    let numOfValleys = -1;
+    let numOfValleys = 0;
     for (let i = 0; i < path.length; i++) {
-        console.log(path[i], heightNum)
-        if (path[i] === "U") {
-            heightNum++;
-        } if (path[i] === "D") {
-            heightNum--;
-        } if (path[i-1] === "U" && heightNum == 0) {
-            numOfValleys++;
+        switch (path[i]) {
+            case "U":
+                heightNum++;
+                break;
+            case "D":
+                heightNum--;
+                break;
         }
+        if ((path[i] === "U") && (heightNum == 0)) {
+            numOfValleys++;
+        } 
     }
     return numOfValleys;
 }
-
 console.log(countingValleys(0, "DDUUDDUDUUUD"));
